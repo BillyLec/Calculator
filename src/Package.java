b28f70c82d7a6ea5b1ec8e0141062c26abd00021
Mme Leclerc-Istria Ombeline <leclerc-@insa-10221>
2017-11-17 10:49:02 +0100

    Debut TD

---
+++ src/Package.java
@@ -0,0 +1,15 @@
+public class Package {
+
+    public Package(int height, int width, int depth, int weight) {
+
+        // ...
+
+    }
+
+    public double calculateLocalShippingCost(){
+
+        // Retourne les frais de port en France
+
+    }
+
+}
---
+++ src/ShippingCostCalculator.java
@@ -0,0 +1,9 @@
+public class ShippingCostCalculator {
+
+    public double calculateShippingCost(Package myPackage, Destination destination){
+
+        // Retourne les frais de port du package en fonction de la destination
+
+    }
+
+}
